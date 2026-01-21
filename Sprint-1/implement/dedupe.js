322bc9e526c5de8ec59e6d1a6f7f8555616cbe18
OCC-L0125 <joseph.yaw.ayitey@gmail.com>
2026-01-21 15:03:24 +0100

    The implemented function have been stated

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,15 @@
-function dedupe() {}
+
+function dedupe(array) {
+  const result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (!result.includes(array[i])) {
+      result.push(array[i]);
+    }
+  }
+
+  return result;
+}
+
+
+module.exports = dedupe;
