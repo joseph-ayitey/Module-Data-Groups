4f718b44e865591ed463d82531f7751e7c6fa2f3
OCC-L0125 <joseph.yaw.ayitey@gmail.com>
2026-01-21 15:52:01 +0100

    i have been able to implement the function to run sum.test.js

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,11 @@
-function sum(elements) {
+function sum(array) {
+  return array.reduce((total, value) => {
+    if (typeof value === "number" && !Number.isNaN(value)) {
+      return total + value;
+    }
+    return total;
+  }, 0);
 }
+
 
 module.exports = sum;
