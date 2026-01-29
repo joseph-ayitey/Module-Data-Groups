5d07ff509ff139d2fa250eedfcca29416821ff55
OCC-L0125 <joseph.yaw.ayitey@gmail.com>
2026-01-29 16:33:02 +0100

    Function, have been implemented to run the tests

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,9 @@
-function contains() {}
+function contains(obj, prop) {
 
+  if (obj === null || typeof obj !== "object" || Array.isArray(obj)) {
+    return false; 
+  }
+
+  return Object.prototype.hasOwnProperty.call(obj, prop);
+}
 module.exports = contains;
