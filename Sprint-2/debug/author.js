c231c0691a9398df59b57f40c04a3f64f44fba82
OCC-L0125 <joseph.yaw.ayitey@gmail.com>
2026-01-29 16:09:40 +0100

    Fix object iteration by using Object.values()

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,7 +1,10 @@
 // Predict and explain first...
+//This code will throw an error and nothing will be logged.
 
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
+
+//The for...of loop only works on iterable things like arrays, strings, or other objects that implement the iterable protocol.
 
 const author = {
   firstName: "Zadie",
@@ -10,7 +13,8 @@ const author = {
   age: 40,
   alive: true,
 };
+// to fix this, i am using the object.values()
 
-for (const value of author) {
+for (const value of Object.values(author)){
   console.log(value);
 }
