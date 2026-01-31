a326fafbb91f08560f6ea1dd4c254548d5375666
OCC-L0125 <joseph.yaw.ayitey@gmail.com>
2026-01-31 10:53:25 +0100

    till function have been stated and created a testing file to run some cases

--- Sprint-2/stretch/till.js
+++ Sprint-2/stretch/till.js
@@ -7,20 +7,20 @@
 function totalTill(till) {
   let total = 0;
 
-  for (const [coin, quantity] of Object.entries(till)) {
-    total += coin * quantity;
+ for (const [coin, quantity] of Object.entries(till)) {
+    const valueInPence = parseInt(coin, 10);
+    total += valueInPence * quantity;
   }
 
-  return `£${total / 100}`;
+  return `£${(total / 100).toFixed(2)}`;
 }
 
-const till = {
-  "1p": 10,
-  "5p": 6,
-  "50p": 4,
-  "20p": 10,
-};
-const totalAmount = totalTill(till);
+
+module.exports = totalTill;
+
+
+
+
 
 // a) What is the target output when totalTill is called with the till object
 
