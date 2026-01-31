e7d84f2683dfe33d562721fa7ea61adf8b23c6f6
OCC-L0125 <joseph.yaw.ayitey@gmail.com>
2026-01-31 11:03:25 +0100

    Till.js file have been modify

--- Sprint-2/stretch/till.js
+++ Sprint-2/stretch/till.js
@@ -25,7 +25,11 @@ module.exports = totalTill;
 // a) What is the target output when totalTill is called with the till object
 
 // b) Why do we need to use Object.entries inside the for...of loop in this function?
+      //Object.entries(till) converts the object into an array of [key, value] pairs
+      //This allows for...of to iterate over each coin and its quantity
 
 // c) What does coin * quantity evaluate to inside the for...of loop?
+      //coin is a string key from the object, like "1" or "50".
 
 // d) Write a test for this function to check it works and then fix the implementation of totalTill
+    //test have been added to the testing file.
